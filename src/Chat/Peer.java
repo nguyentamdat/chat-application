d496f0c87dce70f67361169a59825639932a5e0d
nguyentamdat <nguyentamdat@gmail.com>
2019-10-24 22:23:57 +0700

    Initial commit

---
+++ src/Chat/Peer.java
@@ -0,0 +1,17 @@
+package Chat;
+
+import java.net.Socket;
+
+public class Peer implements Runnable {
+
+    private Socket _socket;
+
+    public Peer() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ src/ChatGUI/Controller.java
@@ -0,0 +1,4 @@
+package ChatGUI;
+
+public class Controller {
+}
