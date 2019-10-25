4e4fbc65b0c21ffac30b2ffee1194e1ca4097dc1
nguyentamdat <nguyentamdat@gmail.com>
2019-10-25 16:58:35 +0700

    add class Message

---
+++ src/Chat/Message.java
@@ -0,0 +1,27 @@
+package Chat;
+
+public class Message {
+    private String type;
+    private Object content;
+
+    public  Message(String type, Object content) {
+        setContent(content);
+        setType(type);
+    }
+
+    public void setContent(Object content) {
+        this.content = content;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Object getContent() {
+        return content;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
