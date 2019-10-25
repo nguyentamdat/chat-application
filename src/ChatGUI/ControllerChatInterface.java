57d400f339a7b6d6c6133a8ec7f007f7771ed74b
Nguyễn Việt Long <nvietlong.1702@gmail.com>
2019-10-25 10:33:21 +0700

    login success

--- src/ChatGUI/ControllerChatInterface.java
+++ src/ChatGUI/ControllerChatInterface.java
@@ -1,4 +1,13 @@
 package ChatGUI;
 
-public class ControllerChatInterface {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerChatInterface implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
