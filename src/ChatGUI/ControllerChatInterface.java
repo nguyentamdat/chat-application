4bb700c89b31c556a56fc7259bf6bd18b259532a
Nguyễn Việt Long <nvietlong.1702@gmail.com>
2019-10-25 10:37:00 +0700

    Revert "login success"
    This reverts commit 57d400f3

--- src/ChatGUI/ControllerChatInterface.java
+++ src/ChatGUI/ControllerChatInterface.java
@@ -1,13 +1,4 @@
 package ChatGUI;
 
-import javafx.fxml.Initializable;
-
-import java.net.URL;
-import java.util.ResourceBundle;
-
-public class ControllerChatInterface implements Initializable {
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-
-    }
+public class ControllerChatInterface {
 }
