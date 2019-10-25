2f8d0083f145049f06a3f25fc6b0ff32eaad17a0
Nguyễn Việt Long <nvietlong.1702@gmail.com>
2019-10-25 10:44:39 +0700

    Login success

--- src/ChatGUI/Main.java
+++ src/ChatGUI/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         primaryStage.setTitle("Chat App");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
     }
 
